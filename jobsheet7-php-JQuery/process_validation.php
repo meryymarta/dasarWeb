c810972d21c0307e6d740a9bc100fe70a390fa83
meryymarta <meriammartadinata@gmail.com>
2025-10-10 11:59:28 +0700

    jobsheet 7

---
+++ jobsheet7-php-JQuery/process_validation.php
@@ -0,0 +1,37 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST["nama"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $errors = array();
+
+    //validasi nama
+    if (empty($nama)){
+        $errors[] = "Nama harus diisi";
+    }
+
+    //validasi email
+    if(empty($email)){
+        $errors[] = "Email harus diisi";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Format email tidak valid";
+
+    }
+
+    if(empty($password)){
+        $error[] = "Passowrd harus diisi";
+    }
+    
+    // Jika ada kesalahan validasi
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+    }
+    } else {
+    // Lanjutkan dengan pemrosesan data jika semua validasi berhasil
+    // Misalnya, menyimpan data ke database atau mengirim email
+    echo "Data berhasil dikirim: Nama = $nama, Email = $email, Password = $password";
+}
+
+}
+?>
